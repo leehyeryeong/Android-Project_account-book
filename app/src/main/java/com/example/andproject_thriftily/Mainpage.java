6336ddd45f658cfa1134049fa75d5b0e339e0ece
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-16 00:34:18 +0900

    시작 화면 구현, 메인 페이지에 캘린더 삽입

---
+++ app/src/main/java/com/example/andproject_thriftily/Mainpage.java
@@ -0,0 +1,14 @@
+package com.example.andproject_thriftily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
+}
