01fba73f0ef33b37227b77f37552df649ac8aa2b
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-23 02:17:34 +0900

    시작 화면 파일 이름 변경, 메인 화면 달력 수정

--- app/src/main/java/com/example/andproject_thriftily/MainActivity.java
+++ app/src/main/java/com/example/andproject_thriftily/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Mainpage extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainpage);
+        setContentView(R.layout.activity_main);
     }
 }
