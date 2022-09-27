b5683c3dd2d0dfdab566189b1aa8d3b9c0f4f66c
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-27 18:52:09 +0900

    지출 패턴 만들기 시작

---
+++ app/src/main/java/com/example/andproject_thriftily/ExpensesPatternActivity.java
@@ -0,0 +1,14 @@
+package com.example.andproject_thriftily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpensesPatternActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expenses_pattern);
+    }
+}
