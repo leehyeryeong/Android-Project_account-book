b8547d6a076402d29c7008254ece83448bf86bde
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-18 12:29:25 +0900

    파일들 수정 및 일일 수입/지출 입력 및 리스트 화면 만들기

--- app/src/main/java/com/example/andproject_thriftily/ExpensesPatternActivity.java
+++ app/src/main/java/com/example/andproject_thriftily/ExpensesPatternActivity.java
@@ -1,14 +1,15 @@
 package com.example.andproject_thriftily;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ExpensesPatternActivity extends AppCompatActivity {
+public class ExpensesPatternActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.expenses_pattern);
-    }
 }
