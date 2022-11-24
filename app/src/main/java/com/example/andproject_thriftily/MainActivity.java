482d74c673b9d699b79d0b905b6a49d795389205
leehyeryeong <w2108@e-mirim.hs.kr>
2022-11-24 18:20:09 +0900

    bottom nav 재생성

--- app/src/main/java/com/example/andproject_thriftily/MainActivity.java
+++ app/src/main/java/com/example/andproject_thriftily/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.andproject_thriftily;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-    TabLayout tabLay;
-    ViewPager viewPager;
-    FragmentAdapter fragAdt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tabLay = findViewById(R.id.tabLay);
-        viewPager = findViewById(R.id.viewPager);
-        fragAdt = new FragmentAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(fragAdt);
-        tabLay.setupWithViewPager(viewPager);
-        tabLay.getTabAt(0).setText("입금 내역");
-        tabLay.getTabAt(1).setText("지출 내역");
+
     }
 }
