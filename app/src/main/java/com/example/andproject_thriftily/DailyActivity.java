6b0683b2a2b874c00d9ed6c6123c94173b6288d9
leehyeryeong <w2108@e-mirim.hs.kr>
2022-11-29 09:14:20 +0900

    bottom nav 구현, 상단에 뜨는 거 수정해야됨

--- app/src/main/java/com/example/andproject_thriftily/DailyActivity.java
+++ app/src/main/java/com/example/andproject_thriftily/DailyActivity.java
@@ -1,4 +1,21 @@
 package com.example.andproject_thriftily;
 
 public class DailyActivity {
+    String date;
+    String content;
+    String coast;
+
+//    public daily(String date, String content, String coast){
+//        this.date = date;
+//        this.content = content;
+//        this.coast = coast;
+//    }
+    public String getDate() {return date;}
+    public void setDate(String date) {this. date = date;}
+
+    public String getContent() {return content;}
+    public void setContent(String content) {this. content = content;}
+
+    public String getCoast() {return coast;}
+    public void setCoast(String coast) {this. coast = coast;}
 }
